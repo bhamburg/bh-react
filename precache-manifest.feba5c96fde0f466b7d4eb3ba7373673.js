771e5a82a56cf26ed235757574c5e7095bde31ae
Brian Hamburg <brian.hamburg@jefferson.edu>
2018-10-18 22:52:41 -0400

    Updates

---
+++ precache-manifest.feba5c96fde0f466b7d4eb3ba7373673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bh-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b06774eda2058450b957",
+    "url": "/bh-react/static/js/runtime~main.b06774ed.js"
+  },
+  {
+    "revision": "e7f1d61d17d483412731",
+    "url": "/bh-react/static/js/main.e7f1d61d.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/bh-react/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "e7f1d61d17d483412731",
+    "url": "/bh-react/static/css/main.f72d33ea.chunk.css"
+  },
+  {
+    "revision": "c7be8951b1df3a79d168ab685bd3fe03",
+    "url": "/bh-react/index.html"
+  }
+];
